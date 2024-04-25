98021997b336f0a7f95d79e4faa389819f34655f
Steezyy17 <steveb1701@outlook.com>
2024-04-25 01:03:30 +0100

    update
    bin added and some UI setup

---
+++ Assets/Scripts/Interactables/BinInteract.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using UnityEngine;
+
+public class BinInteract : MonoBehaviour
+{
+
+    [SerializeField]
+    private Animator binAnim;
+
+    [SerializeField]
+    private GameObject binUI;
+
+    private void Awake()
+    {
+        binUI.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            binAnim.Play("BinOpen");
+            binUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            binAnim.Play("BinClose");
+            binUI.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/ObjectGrabbable.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectGrabbable : MonoBehaviour
+{
+
+    private Rigidbody trashBagRB;
+    private Transform objectGrabPointTransform;
+
+    private Collider myCollider;
+
+    private void Awake()
+    {
+        trashBagRB = GetComponent<Rigidbody>();
+        myCollider = GetComponent<Collider>();
+    }
+
+    public void GrabObject(Transform objectGrabPointTransform)
+    {
+        this.objectGrabPointTransform = objectGrabPointTransform;
+        trashBagRB.useGravity = false; //disables the trash from falling
+        
+    }
+
+    public void DropObject()
+    {
+        this.objectGrabPointTransform = null;
+        trashBagRB.useGravity = true;
+        myCollider.enabled = true;
+    }
+
+    private void Update()
+    {
+        if (objectGrabPointTransform != null)
+        {
+            float lerpValue = 10f;
+            Vector3 moveToNewPosition = Vector3.Lerp(transform.position, objectGrabPointTransform.position, Time.deltaTime * lerpValue); //lerps from world position to hand
+            myCollider.enabled = false;
+            transform.rotation = objectGrabPointTransform.rotation;
+            trashBagRB.MovePosition(objectGrabPointTransform.position);
+        }
+    }
+}
