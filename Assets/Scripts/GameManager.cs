e2f5d92a80cef275e05dc0c1363247d6557bcbd1
Steezyy17 <steveb1701@outlook.com>
2024-04-30 23:38:20 +0100

    home update
    object spawning working

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,42 +7,53 @@ using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
-    [Header("= Trash Manager =")]
-    [Space(15)]
-
     [SerializeField]
     private GameObject[] trashObjects;
 
     [SerializeField]
-    private Transform[] spawnPositions;
+    private Transform[] spawnPositions; //spawn locations
+
+        [Header("= Spawn Values =")]
+        [Space(15)]
 
     [SerializeField]
     private int spawnAmount;
+    [Space(10)]
 
     [SerializeField]
-    private int randSpawnSpot;
+    private int minToSpawn;
 
-    private void Start()
+    [SerializeField]
+    private int maxToSpawn;
+
+    //private int amountAlreadySpawned;
+
+    private void Awake()
     {
         if (trashObjects.Length == 0) Debug.LogError("No trash assigned, assign some in the inspector");
-        if (spawnPositions.Length == 0) Debug.LogError("No spawn positions assigned");
-
-
+        if (spawnPositions.Length == 0) Debug.LogError("No spawn positions assigned");   
     }
 
-    private void Update()
+    private void Start()
     {
         SpawnTrash();
     }
 
     private void SpawnTrash()
     {
-        randSpawnSpot = Random.Range(0, spawnPositions.Length);
-
-        for (int i = 0; i < trashObjects.Length; i++)
+        spawnAmount = Random.Range(minToSpawn, maxToSpawn);
+        // try and make it so that only 1 object is spawned at each position
+        for (int i = 0; i < spawnAmount; i++)
         {
-            Instantiate(trashObjects[i - 1], spawnPositions[randSpawnSpot].transform.position, Quaternion.identity);
-            //spawnAmount++;
+            Instantiate(trashObjects[Random.Range(0, trashObjects.Length)], spawnPositions[Random.Range(0, spawnPositions.Length)].transform.position, Quaternion.identity);
         }
+
+        //spawnAmount = Random.Range(minToSpawn, maxToSpawn);
+
+        //while (amountAlreadySpawned < spawnAmount)
+        //{
+        //    Instantiate(trashObjects[Random.Range(0, trashObjects.Length)], spawnPositions[Random.Range(0, spawnPositions.Length)].transform.position, Quaternion.identity);
+        //    amountAlreadySpawned++;
+        //}
     }
 }
