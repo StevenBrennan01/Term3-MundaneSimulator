99cccbba0bd1fbf73907abb54a47f3f4fc5ff52b
Steezyy17 <steveb1701@outlook.com>
2024-05-14 10:57:18 +0100

    small changes

---
+++ Assets/UIManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject controlsUI;
+    [SerializeField] private GameObject scoreUI;
+    [SerializeField] private GameObject timerUI;
+    [SerializeField] private GameObject crosshairUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
