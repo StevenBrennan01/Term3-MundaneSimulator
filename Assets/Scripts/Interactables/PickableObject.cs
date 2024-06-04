476a883a308461b43bc72adb4533eca74c3dc239
Steezyy17 <steveb1701@outlook.com>
2024-06-04 16:48:46 +0100

    some post submission changes

--- Assets/Scripts/Interactables/PickableObject.cs
+++ Assets/Scripts/Interactables/PickableObject.cs
@@ -21,6 +21,7 @@ public class PickableObject : MonoBehaviour
     #endregion
 
     [SerializeField] private GameObject ballItem;
+
     [Space(15)]
 
     [SerializeField] private GameObject[] binTrash;
